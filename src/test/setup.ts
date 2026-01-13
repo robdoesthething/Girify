0f2d6c25744b66c0f77b6f6c56c9dd2c57b1f1b6
Roberto Sánchez Gallego <robertohacecosas@gmail.com>
2026-01-13 21:38:05 +0100

    style: fix code formatting issues
    Applied prettier --write to resolve format:check failures in test and type definition files.

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,3 @@
-
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
@@ -6,5 +5,5 @@ import * as matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
 
 afterEach(() => {
-    cleanup();
+  cleanup();
 });
